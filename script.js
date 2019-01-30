6116ad41c91f0ee29a8f9ee40872d609891b4ce7
Adam Frank White <adamfrankwhite@gmail.com>
2019-01-30 22:59:18 +0000

    Improved icons and functionality, added presentation

--- script.js
+++ script.js
@@ -35,13 +35,81 @@ var punctuation_questions = [ //array of objects as easier to maintain
 	}
 ]
 
+const grammar_questions = [ //array of objects as easier to maintain
+
+	{
+		question: "Select the adverb",
+		choices: ["quickly", "running", "hitchiker", "be"],
+		answer: "quickly"
+	},
+	{
+		question: "Select the subordinate clause",
+		choices: ["Although it was raining,", "I am very happy", "a blue car", "up and down"],
+		answer: "Although it was raining,"
+	},
+
+	{
+		question: "Select the correctly punctuated sentence",
+		choices: ["I want to get a skateboard a puppy and some ice cream.", "Why bother learning punctuation.", "Should I eat this strange-looking thing I found on the floor?", "Alice is a nice person although she smells like elderberries."],
+		answer: "Should I eat this strange-looking thing I found on the floor?"
+	},
+
+	{
+		question: "What is an ellipsis?",
+		choices: ["&", "*", "...", "^"],
+		answer: "..."
+	},
+
+	{
+		question: "What can we use a colon (:) for?",
+		choices: ["To introduce a list", "To introduce an explanation", "To introduce a quote", "All of the above"],
+		answer: "All of the above"
+	}
+]
+
+const comprehension_questions = [ //array of objects as easier to maintain
+
+	{
+		question: "What is the flight speed of the swallow?",
+		choices: ["10mph", "100mph", "1000mph", "Is that an African or European swallow?"],
+		answer: "Is that an African or European swallow?"
+	},
+	{
+		question: "Select the subordinate clause",
+		choices: ["Although it was raining,", "I am very happy", "a blue car", "up and down"],
+		answer: "Although it was raining,"
+	},
+
+	{
+		question: "Select the correctly punctuated sentence",
+		choices: ["I want to get a skateboard a puppy and some ice cream.", "Why bother learning punctuation.", "Should I eat this strange-looking thing I found on the floor?", "Alice is a nice person although she smells like elderberries."],
+		answer: "Should I eat this strange-looking thing I found on the floor?"
+	},
+
+	{
+		question: "What is an ellipsis?",
+		choices: ["&", "*", "...", "^"],
+		answer: "..."
+	},
+
+	{
+		question: "What can we use a colon (:) for?",
+		choices: ["To introduce a list", "To introduce an explanation", "To introduce a quote", "All of the above"],
+		answer: "All of the above"
+	}
+]
+
+
+// ===== Functions ===== //
 function createQuestion(questionSetCount) {
 	let q_set = punctuation_questions[questionSetCount];
 	$('.quiz').append('<h3>' + q_set.question + '</h3>') //create question heading
+	$('.quiz').append('<h4>' + (questionSetCount+1) + ' / 5</h4>')
 	for (let i=0; i<q_set.choices.length; i += 1) {
 		$('.quiz').append('<p class=' + i +'>' + q_set.choices[i] + '</p>')
 	}
-	$('.quiz').append('<button class="next-btn">Next Question</button>')
+
+
 
 
 }
@@ -55,7 +123,7 @@ function getAnswer(q_num){
 $('#go').one('click', function () {
 	$('.quiz').css({"border": "3px black dotted"})
 	const $quiz_selection = $('.choice').val();
-	if ($quiz_selection === "punc") {
+	if ($quiz_selection === "punctuation_questions") {
 		createQuestion(0);
 		checkAnswer();
 
@@ -71,7 +139,7 @@ function checkAnswer () {
 		// if the correct answer chosen, highlighted green and wrong answers highlighted red
 		if (answer == getAnswer(questionSetCount)) {
 			(event.target).setAttribute('class', 'correct')
-			$(event.target).append('<img src="images/correct.jpg" class="float_right" width="35px">')
+			$(event.target).append('<img src="images/correct.jpg" width="25px">')
 			correctAnswers += 1;
 			// if incorrect answer, all wrong answers selected red, correct answer shown in green
 		} else {
@@ -80,28 +148,47 @@ function checkAnswer () {
 			for (i=0; i<answer_choices.length; i+=1) {
 				if (answer_choices[i].textContent == getAnswer(questionSetCount)) {
 					answer_choices[i].setAttribute('class', 'correct')
-					$(answer_choices[i]).append('<img src="images/correct.jpg" class="float_right" width="35px">')
+					$(answer_choices[i]).append('<img src="images/correct.jpg" width="25px">')
 			} else {
 					answer_choices[i].setAttribute('class', 'wrong')
-					$(answer_choices[i]).append('<img src="images/incorrect.jpg" class="float_right" width="40px">')
+					$(answer_choices[i]).append('<img src="images/incorrect.jpg" width="25px">')
 			}
-			(event.target).setAttribute('class', 'wrong')
+
+
 
 
 		}
 }
 		questionSetCount += 1;
-		console.log(correctAnswers);
+		if (questionSetCount < punctuation_questions.length) {
+			$('.quiz').append('<button class="next-btn">NEXT QUESTION</button>') // better design for button to appear after you select answer, not automatically
+
+		} else {
+			$('.quiz').append('<button class="next-btn">Score</button>') // shows results button on final answer select
+		}
+		// console.log(correctAnswers);
+		console.log(questionSetCount);
 	});
 }
 
 
 
 $(document).on('click', '.next-btn', function () {
-	$('.quiz').empty();
-	createQuestion(questionSetCount);
-	checkAnswer(); // need to change to part of main function, with an option only allowing it to be clicked, once answer selected, currently it is simply refreshing the box contents, with the same questions
+
+	if (questionSetCount != punctuation_questions.length) {
+		$('.quiz').empty();
+		createQuestion(questionSetCount);
+
+		checkAnswer(); // need to change to part of main function, with an option only allowing it to be clicked, once answer selected, currently it is simply refreshing the box contents, with the same questions
+	} else {
+		$('.quiz').empty();
+		$('.quiz').append('<h2>You achieved a score of ' + correctAnswers + '</h2')
+		$('.quiz').append('<img src="images/trophy.jpg" width="200px">') // add conditional to include different ranks
+	}
+
 });
+// sort out final score screen
+
 
 
 // change answer valuable to dynamic variable
