ad25687f0bddb5b9c9d5ae0bdebccceab7dd0b59
Adam Frank White <adamfrankwhite@gmail.com>
2019-03-21 20:47:35 +0000

    refactoring

--- script.js
+++ script.js
@@ -3,132 +3,6 @@ $(document).ready(function () {
 // ==== Questions ==== // put in separate script file
 var correctAnswers = 0; // create correct guess counter#
 var questionNumber = 0;
-var punctuation_questions = [ //array of objects as easier to maintain
-
-	{
-		question: "What is the purpose of a comma?",
-		choices: ["to separate clauses", "to indicate a pause", "to separate items in a list", "All of the above"],
-		answer: "All of the above"
-	},
-	{
-		question: "Select the subordinate clause",
-		choices: ["Although it was raining,", "I am very happy", "a blue car", "up and down"],
-		answer: "Although it was raining,"
-	},
-
-	{
-		question: "Select the correctly punctuated sentence",
-		choices: ["I want to get a skateboard a puppy and some ice cream.", "Why bother learning punctuation.", "Should I eat this strange-looking thing I found on the floor?", "Alice is a nice person although she smells like elderberries."],
-		answer: "Should I eat this strange-looking thing I found on the floor?"
-	},
-
-	{
-		question: "What is an ellipsis?",
-		choices: ["&", "*", "...", "^"],
-		answer: "..."
-	},
-
-	{
-		question: "What can we use a colon (:) for?",
-		choices: ["To introduce a list", "To introduce an explanation", "To introduce a quote", "All of the above"],
-		answer: "All of the above"
-	},
-	
-	{
-		question: "What is a cat?",
-		choices: ["some sort of breakfast cereal", "a type of car", "a digging implement", "a furry master"],
-		answer: "a furry master"
-	},
-	
-	{
-		question: "Which of these is a vegetable?",
-		choices: ["pelican", "strawberry", "swimming pool", "carrot"],
-		answer: "carrot"
-	},
-	
-	{
-		question: "Spot the punctuation error",
-		choices: ["Piers Morgan is a vile human being.", "Piers Morgan is unattractive: both inside and out.", "Piers Morgan, a poor excuse for a human being, has no soul.", "What is the point of Piers Morgan!"],
-		answer: "What is the point of Piers Morgan!"
-	},
-	
-	{
-		question: "Spot the noun",
-		choices: ["sparrow", "never", "incredible", "surfing"],
-		answer: "sparrow"
-	},
-	
-	{
-		question: "What is a pronoun?",
-		choices: ["a delicacy native to the island of Zanzibar", "a word that takes the place of a noun", "a month of the year", "a unit of speed"],
-		answer: "a word that takes the place of a noun"
-	}
-]
-
-const grammar_questions = [ //array of objects as easier to maintain
-
-	{
-		question: "Select the adverb",
-		choices: ["quickly", "running", "hitchiker", "be"],
-		answer: "quickly"
-	},
-	{
-		question: "Select the subordinate clause",
-		choices: ["Although it was raining,", "I am very happy", "a blue car", "up and down"],
-		answer: "Although it was raining,"
-	},
-
-	{
-		question: "Select the correctly punctuated sentence",
-		choices: ["I want to get a skateboard a puppy and some ice cream.", "Why bother learning punctuation.", "Should I eat this strange-looking thing I found on the floor?", "Alice is a nice person although she smells like elderberries."],
-		answer: "Should I eat this strange-looking thing I found on the floor?"
-	},
-
-	{
-		question: "What is an ellipsis?",
-		choices: ["&", "*", "...", "^"],
-		answer: "..."
-	},
-
-	{
-		question: "What can we use a colon (:) for?",
-		choices: ["To introduce a list", "To introduce an explanation", "To introduce a quote", "All of the above"],
-		answer: "All of the above"
-	}
-]
-
-const comprehension_questions = [ //array of objects as easier to maintain
-
-	{
-		question: "What is the flight speed of the swallow?",
-		choices: ["10mph", "100mph", "1000mph", "Is that an African or European swallow?"],
-		answer: "Is that an African or European swallow?"
-	},
-	{
-		question: "Select the subordinate clause",
-		choices: ["Although it was raining,", "I am very happy", "a blue car", "up and down"],
-		answer: "Although it was raining,"
-	},
-
-	{
-		question: "Select the correctly punctuated sentence",
-		choices: ["I want to get a skateboard a puppy and some ice cream.", "Why bother learning punctuation.", "Should I eat this strange-looking thing I found on the floor?", "Alice is a nice person although she smells like elderberries."],
-		answer: "Should I eat this strange-looking thing I found on the floor?"
-	},
-
-	{
-		question: "What is an ellipsis?",
-		choices: ["&", "*", "...", "^"],
-		answer: "..."
-	},
-
-	{
-		question: "What can we use a colon (:) for?",
-		choices: ["To introduce a list", "To introduce an explanation", "To introduce a quote", "All of the above"],
-		answer: "All of the above"
-	}
-]
-
 
 // ===== Functions ===== //
 
@@ -178,10 +52,6 @@ function guessAttempt () {
 					answerChoices[i].setAttribute('class', 'wrong')
 					$(answerChoices[i]).append('<img class="mark" src="images/incorrect.jpg">')
 			}
-
-
-
-
 		}
 }
 		questionNumber += 1;
@@ -194,9 +64,7 @@ function guessAttempt () {
 	});
 }
 
-
-
-// Event Listeners =========
+// ==== Event Listeners ====
  
 $('#go').on('click', function () {
 		$('.quiz').css({"border": "3px black solid", "background-color": "white"}) // must set border in JavaScript, else empty border div before script loads
